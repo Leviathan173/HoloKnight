ba6d6d9309aac30e53b7da122a807af9ae2be4e5
Leviathan173 <hft0149@Gmail.com>
2020-04-30 00:02:55 +0800

    添加Intro界面&&添加音频素材

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -1,10 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField] SettingPopup Setting;
+    [SerializeField] Canvas MainCanvas;
+    [SerializeField] AudioSource BGM;
+
     public void OnExitGame() {
         Application.Quit();
+    }
+
+    public void OnNewGame() {
+        // TODO 异步加载
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void OnLoadGame() {
+        // TODO 加载游戏
+        // 需要一个弹出界面
+    }
+
+    public void LoadGame(string name) {
+        // TODO 加载特定的一个存档
+        // 传的参数没有决定
+    }
+
+    public void OnSettingOpen() {
+        Setting.Open();
+        MainCanvas.gameObject.SetActive(false);
+    }
+
+    public void OnSettingExit() {
+        Setting.Close();
+        MainCanvas.gameObject.SetActive(true);
+    }
+
+    public void OnSliderValueChanged(Slider slider) {
+        if (slider.name.Contains("SE")) {
+
+        } else {
+            BGM.volume = slider.value;
+        }
     }
 }
