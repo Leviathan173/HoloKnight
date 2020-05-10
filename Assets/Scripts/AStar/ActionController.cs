2bde2ecf2e5cd4eb7cf700de628117b6f1677197
Leviathan173 <hft0149@Gmail.com>
2020-05-10 16:28:33 +0800

    写了一点行动模式的入口

--- Assets/Scripts/AStar/ActionController.cs
+++ Assets/Scripts/AStar/ActionController.cs
@@ -5,16 +5,49 @@ using UnityEngine;
 
 public class ActionController : MonoBehaviour
 {
+    public enum ActionMode {
+        Aggressive,
+        Defence
+    }
     // UNDONE 完成行动模式
     // 分为进攻和防守两个模式，防守移动较少会等待玩家进入攻击距离然后再发起进攻
     // 进攻会积极的接近距离发动攻击
     // 还有一个观望或者叫做牵扯模式
     // 在精力槽变少时稍微拉开距离等待精力回复，回复之后继续按照上述模式行动
 
-
+    public bool hasCorountine { get; set; }
+    IEnumerator coroutine;
 
     void Start() {
-
+        hasCorountine = false;
     }
 
+    public void StartAction(EnemyManager manager, ActionMode mode) {
+        if (mode == ActionMode.Aggressive)
+            coroutine = ActionAggressive(manager);
+        else
+            coroutine = ActionDefence(manager);
+        StartCoroutine(coroutine);
+        hasCorountine = true;
+    }
+    public void StopAction() {
+        if (coroutine != null) {
+            StopCoroutine(coroutine);
+        } else {
+            print("has no Action started");
+        }
+        hasCorountine = false;
+    }
+
+
+    IEnumerator ActionAggressive(EnemyManager manager) {
+        float Hp = manager.currentHealth;
+        float Sp = manager.currentStamina;
+        yield return null;
+    }
+    IEnumerator ActionDefence(EnemyManager manager) {
+        float Hp = manager.currentHealth;
+        float Sp = manager.currentStamina;
+        yield return null;
+    }
 }
--- Assets/Scripts/AStar/PathFollower.cs
+++ Assets/Scripts/AStar/PathFollower.cs
@@ -4,13 +4,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PathFollower : MonoBehaviour {
-    PathFinderData finder;
-    float deltaX;
     public bool hasCoroutine { get; set; }
     IEnumerator coroutine;
     void Start() {
-        finder = PathFinderData.Instance;
-        deltaX = 3;
+        hasCoroutine = false;
     }
 
     void Update() {
@@ -20,10 +17,6 @@ public class PathFollower : MonoBehaviour {
     }
 
     public void FollowPath(List<Node> nodes, EnemyManager manager) {
-        print("fp manager name:" + manager.name);
-        foreach (var node in nodes) {
-            print("node in follower :" + node.ID);
-        }
         coroutine = Follower(nodes, manager);
         hasCoroutine = true;
         StartCoroutine(coroutine);
@@ -34,7 +27,7 @@ public class PathFollower : MonoBehaviour {
             StopCoroutine(coroutine);
             hasCoroutine = false;
         } else {
-            print("has no coroutine started...");
+            print("has no Follower started...");
         }
 
     }
@@ -84,29 +77,29 @@ public class PathFollower : MonoBehaviour {
     IEnumerator Follower(List<Node> nodes, EnemyManager manager) {
         Vector3 goal = nodes[0].Position;
         for (int i = 0; i < nodes.Count; i++) {
-            print("pf loop " + i + "name:" + manager.name);
-            print("pf node id " + nodes[i].ID + "name:" + manager.name);
+            //print("pf loop " + i + "name:" + manager.name);
+            //print("pf node id " + nodes[i].ID + "name:" + manager.name);
             Node a = nodes[i];
             if (nodes.Count == 1) {
-                print("pf only one node" + " name:" + manager.name);
+                //print("pf only one node" + " name:" + manager.name);
                 goal = a.Position;
             } else {
                 if (i + 1 == nodes.Count) {
-                    print("pf reach last node" + " name:" + manager.name);
+                    //print("pf reach last node" + " name:" + manager.name);
                     continue;
                 }
                 Node b = nodes[i + 1];
                 if (IsFlat(a, b)) {
-                    print("pf is flat" + " name:" + manager.name);
+                    //print("pf is flat" + " name:" + manager.name);
                     goal.x += b.Position.x - a.Position.x;
-                    print("pf goal " + goal + " name:" + manager.name);
+                    //print("pf goal " + goal + " name:" + manager.name);
                     continue;
                 } else {
-                    print("pf isn't flat" + " name:" + manager.name);
+                    //print("pf isn't flat" + " name:" + manager.name);
                     goal = b.Position;
                     while (!Check(manager.enemy.transform.position, b.Position, manager.name)) {
                         if (Check(manager.enemy.transform.position, Managers.Player.player.transform.position, manager.name)) {
-                            print("pf hit!!!" + " name:" + manager.name);
+                            //print("pf hit!!!" + " name:" + manager.name);
                             goto End;
                         }
                         if (manager.isFacingRight) {
@@ -126,18 +119,18 @@ public class PathFollower : MonoBehaviour {
                         }
                         yield return new WaitForSeconds(0.2f);
                     }
-                    print("pf follow complet" + " name:" + manager.name);
+                    //print("pf follow complet" + " name:" + manager.name);
                     goal = b.Position;
                     continue;
                 }
             }
         }
         Check:
-        print("pf check last " + "name:" + manager.name);
+        //print("pf check last " + "name:" + manager.name);
         while (!Check(manager.enemy.transform.position, goal, manager.name)) {
-            print("pf into last check " + "name:" + manager.name);
+            //print("pf into last check " + "name:" + manager.name);
             if (Check(manager.enemy.transform.position, Managers.Player.player.transform.position, manager.name)) {
-                print("pf hit!!!" + " name:" + manager.name);
+                //print("pf hit!!!" + " name:" + manager.name);
                 goto End;
             }
             if (manager.isFacingRight) {
@@ -164,7 +157,7 @@ public class PathFollower : MonoBehaviour {
             }
         }
         End:
-        print("pf end " + "name:" + manager.name);
+        //print("pf end " + "name:" + manager.name);
         hasCoroutine = false;
         manager.animator.SetFloat(EAParameters.SPEED, -1);
         yield return null;
@@ -174,13 +167,8 @@ public class PathFollower : MonoBehaviour {
         return Mathf.Abs(a.Position.y - b.Position.y) < 1.5f;
     }
 
-    void StartAction() {
-        print("start action");
-
-    }
-
     bool Check(Vector3 currPos, Vector3 goalPos, string tag, float deviationX = 2.5f, float deviationY = 2.5f) {
-        print("pf Checking pos:" + currPos + " pos:" + goalPos + " tag:" + tag);
+        //print("pf Checking pos:" + currPos + " pos:" + goalPos + " tag:" + tag);
         if (Mathf.Abs(currPos.x - goalPos.x) < deviationX) {
             if (Mathf.Abs(currPos.y - goalPos.y) < deviationY) {
                 print("on goal currpos:" + currPos + " goalPos:" + goalPos);
